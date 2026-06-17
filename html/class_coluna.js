df076dbd55ec1eead8f5a93cb81eb563b866115e
Pedro Rafael Faria Ferreira <pedrorafaelfaria5@gmail.com>
2026-06-17 19:46:24 -0300

    Remove main application and associated files, including test cases and dependencies, to clean up the project structure.

--- html/class_coluna.js
+++
@@ -1,5 +0,0 @@
-var class_coluna =
-[
-    [ "getColuna", "class_coluna.html#a637ea01cbc4fe21536589c7cf39f3b8a", null ],
-    [ "setColuna", "class_coluna.html#ab1c74d1f9557695a2df38463a2efb813", null ]
-];
